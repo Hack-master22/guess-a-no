ac0d1bb19919c17af118df5caf2fe4192b42e0b4
Hack-master22 <105119727+Hack-master22@users.noreply.github.com>
2025-01-05 10:20:17 +0530

    Add files via upload

---
+++ guess.js
@@ -0,0 +1,6 @@
+let gamenum = 64;
+let usernum = prompt("enter a num");
+while(gamenum!=usernum){
+    usernum = prompt("guess it again");
+}
+console.log("congratulation you guess it right",gamenum)
